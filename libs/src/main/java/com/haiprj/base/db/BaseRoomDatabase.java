d54510085bd436f989aff9d21c0b16fa4c6248dc
hai161102 <ripallrank@gmail.com>
2023-02-08 10:33:16 +0700

    update database

---
+++ libs/src/main/java/com/haiprj/base/db/BaseRoomDatabase.java
@@ -0,0 +1,11 @@
+package com.haiprj.base.db;
+
+import androidx.annotation.NonNull;
+import androidx.room.DatabaseConfiguration;
+import androidx.room.InvalidationTracker;
+import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteOpenHelper;
+
+public abstract class BaseRoomDatabase extends RoomDatabase {
+
+}
